87dd181bac5efd1a30589edfda326907a23bc642
Rika Okoshi <rika.okoshi@gmail.com>
2015-09-02 09:46:35 +0900

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe "Static pages" do
+  
+  describe "Home page" do
+      
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+  end
+end
+
+RSpec.configure do |config|
+
+  config.include Capybara::DSL
+end
